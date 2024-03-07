20aceea33f164ca51726eb6c2ccc5c79566aade7
Caio Imbroisi <kaikatsue@gmail.com>
2024-03-07 05:37:43 -0300

    updated title

--- src/common/ui/Header.jsx
+++ src/common/ui/Header.jsx
@@ -3,9 +3,10 @@ import React from "react"
  const Header = () => {
     return (
             <header className="bg-sky-950 mb-5 p-8 text-6xl russo-one-regular text-white bg-sky-60 text-center overline">
-                <h2>FLAG</h2>
-                <h2>SEARCH</h2>
-            </header>
+            <h2>FLAG</h2>
+            <h2>SEARCH</h2>
+            <p className="text-sm font-thin	">YOUR COUNTRY INFO</p>
+        </header>
     )
 }
 
